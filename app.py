b61c683f152edd765e0e9c6aee7cd485b7cc33c6
pickleherring <100018622+pickleherring@users.noreply.github.com>
2022-11-22 16:35:47 +0100

    adds filtering by character

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ streamlit.markdown("""
 
 **← check 'selfcest' to also include pairings of characters with themselves**
 
+**← deselect any characters you are not interested in**
+
 **↓ browse the 'rarepairs' and 'zeropairs' tab for rare or nonexistent pairings**
 
 ---
@@ -37,13 +39,21 @@ selected_types = streamlit.sidebar.multiselect(
     default=relationships['type'].unique()
 )
 
+selfcest = streamlit.sidebar.checkbox('selfcest')
+
+selected_characters = streamlit.sidebar.multiselect(
+    'characters',
+    relationships['A'].unique(),
+    default=relationships['A'].unique()
+)
+
+relationships = relationships[relationships['A'].isin(selected_characters) | relationships['B'].isin(selected_characters)]
 relationships = relationships[relationships['type'].isin(selected_types)]
 
-if not streamlit.sidebar.checkbox('selfcest'):
+if not selfcest:
     relationships = relationships[~relationships['selfcest']]
 
-
-if selected_types:
+if selected_types and selected_characters:
 
     streamlit.subheader('results')
 
@@ -51,46 +61,63 @@ if selected_types:
 
     with top_tab:
 
-        top_pairs = relationships.nlargest(n_top_pairs, ['count'])
-        top_pairs = top_pairs.sort_values(['count'])
-        top_pairs['pair'] = top_pairs['A'].str.cat(top_pairs['B'], sep='/')
+        any_pairs = relationships[relationships['count']>0]
 
-        top_pairs['pair'] = pandas.Categorical(
-            top_pairs['pair'],
-            categories=top_pairs['pair'].tolist()
-        )
+        if not any_pairs.empty:
 
-        fig = (
-            plotnine.ggplot(top_pairs)
-            + plotnine.aes(
-                x='pair',
-                y='count',
-                label='count'
+            top_pairs = any_pairs.nlargest(n_top_pairs, ['count'])
+            top_pairs = top_pairs.sort_values(['count'])
+            top_pairs['pair'] = top_pairs['A'].str.cat(top_pairs['B'], sep='/')
+
+            top_pairs['pair'] = pandas.Categorical(
+                top_pairs['pair'],
+                categories=top_pairs['pair'].tolist()
             )
-            + plotnine.geom_col()
-            + plotnine.geom_text()
-            + plotnine.coord_flip()
-        )
 
-        streamlit.pyplot(fig.draw())
+            fig = (
+                plotnine.ggplot(top_pairs)
+                + plotnine.aes(
+                    x='pair',
+                    y='count',
+                    label='count'
+                )
+                + plotnine.geom_col()
+                + plotnine.geom_text()
+                + plotnine.coord_flip()
+            )
+
+            streamlit.pyplot(fig.draw())
+        
+        else:
+            streamlit.markdown('**none!**')
 
     with rare_tab:
 
         rarepairs = relationships[relationships['count']>0].nsmallest(n_rarepairs, ['count'], keep='all')
 
-        for row in rarepairs.itertuples(index=False):
+        if not rarepairs.empty:
 
-            query = parse.quote_plus(ao3.wrangle_relationship_tag(row.A, row.B))
-            url = f'{ao3.url}?{ao3.search_field}="{query}"'
+            for row in rarepairs.itertuples(index=False):
 
-            streamlit.markdown(f'* [{row.A}/{row.B}]({url}) ({row.count})')
+                query = parse.quote_plus(ao3.wrangle_relationship_tag(row.A, row.B))
+                url = f'{ao3.url}?{ao3.search_field}="{query}"'
+
+                streamlit.markdown(f'* [{row.A}/{row.B}]({url}) ({row.count})')
+        
+        else:
+            streamlit.markdown('**none!**')
 
     with zero_tab:
         
         zeropairs = relationships[relationships['count']==0]
+
+        if not zeropairs.empty:
         
-        for row in zeropairs.itertuples(index=False):
-            streamlit.markdown(f'* {row.A}/{row.B}')
+            for row in zeropairs.itertuples(index=False):
+                streamlit.markdown(f'* {row.A}/{row.B}')
+        
+        else:
+            streamlit.markdown('**none!**')
 
 
 streamlit.markdown("""
